c3dd53705dbcf051bbcd198421531bdd85abe92d
Lemonova Mikashiya <lemonovamikashiya@gmail.com>
2025-06-25 19:06:30 +0700

    test push

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:neatzy/view/home.dart';
 //import 'package:neatzy/view/login.dart';
 import 'package:neatzy/view/register.dart';
-//
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
